643b63df83ab686537992fc85dcb5339c5584dd7
Antoine Riviere <aereivir@gmail.com>
2015-06-07 22:57:15 +0200

    handle error if youtube or vimeo

--- app/src/main/java/com/ar/tothestars/ui/APODPhotoView.java
+++ app/src/main/java/com/ar/tothestars/ui/APODPhotoView.java
@@ -19,6 +19,8 @@ import uk.co.senab.photoview.PhotoView;
  */
 public class APODPhotoView extends RelativeLayout {
 
+    public static final String VIMEO = "player.vimeo.com";
+    public static final String YOUTUBE = "www.youtube.com";
     private Photo mPhoto;
 
     private PhotoView mPhotoView;
@@ -56,29 +58,33 @@ public class APODPhotoView extends RelativeLayout {
     public void setModel(Photo photo) {
         mPhoto = photo;
 
-        Picasso.with(getContext())
-                .load(mPhoto.getUrl())
-                .fit()
-                .centerInside()
+        if (!isPhotoValid()) {
+            mErrorMessageView.setVisibility(View.VISIBLE);
+        } else {
+            Picasso.with(getContext())
+                    .load(mPhoto.getUrl())
+                    .fit()
+                    .centerInside()
 //                .transform(PaletteTransformation.instance())
-                .into(mPhotoView, new com.squareup.picasso.Callback.EmptyCallback() {
-                    @Override
-                    public void onSuccess() {
+                    .into(mPhotoView, new com.squareup.picasso.Callback.EmptyCallback() {
+                        @Override
+                        public void onSuccess() {
 //                        //get palette
 //                        Bitmap bitmap = ((BitmapDrawable) mPhotoView.getDrawable()).getBitmap(); // Ew!
 //                        Palette palette = PaletteTransformation.getPalette(bitmap);
 //                        int mutedDark = palette.getDarkMutedColor(0x000000);
 //                        mPhotoView.setBackgroundColor(mutedDark);
 
-                        Log.d("APODPhotoView", "Successfully loaded " + mPhoto.getUrl());
-                    }
+                            Log.d("APODPhotoView", "Successfully loaded " + mPhoto.getUrl());
+                        }
 
-                    @Override
-                    public void onError() {
-                        Log.d("APODPhotoView", "Failed to load " + mPhoto.getUrl());
-                        mErrorMessageView.setVisibility(View.VISIBLE);
-                    }
-                });
+                        @Override
+                        public void onError() {
+                            Log.d("APODPhotoView", "Failed to load " + mPhoto.getUrl());
+                            mErrorMessageView.setVisibility(View.VISIBLE);
+                        }
+                    });
+        }
     }
 
     private void init(Context context) {
@@ -86,5 +92,15 @@ public class APODPhotoView extends RelativeLayout {
 
         mPhotoView = (PhotoView) findViewById(R.id.photo);
         mErrorMessageView = (TextView) findViewById(R.id.error_message);
+    }
+
+    private boolean isPhotoValid() {
+        if (mPhoto.getUrl().contains(VIMEO)) {
+            return false;
+        } else if (mPhoto.getUrl().contains(YOUTUBE)) {
+            return false;
+        }
+
+        return true;
     }
 }
